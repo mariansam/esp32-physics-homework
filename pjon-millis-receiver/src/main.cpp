99d517b79fb8314631dd1761dc96fee0ba30aafc
Marian Šámal <margotka000@gmail.com>
2020-03-28 11:23:31 +0100

    define pin, baudrate

--- pjon-millis-receiver/src/main.cpp
+++ pjon-millis-receiver/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
+#define PIN A5
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
 }
--- pjon-string-transmitter/src/main.cpp
+++ pjon-string-transmitter/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
+#define PIN 5
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
 }
