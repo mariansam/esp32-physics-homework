6ac25bbed0e297f1dce1aa20f68817d2907f737a
Marian Šámal <margotka000@gmail.com>
2020-03-28 12:16:39 +0100

    add digital-transmitter, analog-receiver

--- analog-receiver/src/main.cpp
+++ analog-receiver/src/main.cpp
@@ -4,8 +4,10 @@
 
 void setup() {
     Serial.begin(115200);
+    pinMode(PIN, INPUT);
 }
 
 void loop() {
-
+    Serial.println(analogRead(PIN));
+    delay(200);
 }
--- digital-transmitter/src/main.cpp
+++ digital-transmitter/src/main.cpp
@@ -4,8 +4,15 @@
 
 void setup() {
     Serial.begin(115200);
+    pinMode(PIN, OUTPUT);
 }
 
 void loop() {
-    
+    Serial.println("writing HIGH");
+    digitalWrite(PIN, HIGH);
+    delay(1000);
+
+    Serial.println("writing LOW");
+    digitalWrite(PIN, LOW);
+    delay(1000);
 }
